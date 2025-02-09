3705d5fda40dc8d6cff51f9c54d3d4d8c3019480
ARYAN-NIKNEZHAD <aryan513966@gmail.com>
2025-02-09 06:20:16 +0330

    feat(api): Add base API for models

--- dj_dynamic_form/views.py
+++ dj_dynamic_form/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import DynamicForm, DynamicField, FormSubmission
+from .serializers import DynamicFormSerializer, DynamicFieldSerializer, FormSubmissionSerializer
 
-# Create your views here.
+class DynamicFormViewSet(viewsets.ModelViewSet):
+    """
+    API for managing Dynamic Forms.
+    """
+    queryset = DynamicForm.objects.all()
+    serializer_class = DynamicFormSerializer
+
+class DynamicFieldViewSet(viewsets.ModelViewSet):
+    """
+    API for managing Dynamic Fields inside a form.
+    """
+    queryset = DynamicField.objects.all()
+    serializer_class = DynamicFieldSerializer
+
+class FormSubmissionViewSet(viewsets.ModelViewSet):
+    """
+    API for managing form submissions.
+    """
+    queryset = FormSubmission.objects.all()
+    serializer_class = FormSubmissionSerializer
